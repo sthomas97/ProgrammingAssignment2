c096a7f67669f4a64c3bd4e1dff6c9468477b8a9
sthomas97 <sarahtthomas@gmail.com>
2015-08-23 00:07:41 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My code caches the matrix inverse so that an already calculated inverse doesn't need to be recalculated
+## I essentially used the vestor example code to base my code bc after trying various ways to make it work I 
+## found that to be the simplest
 
-## Write a short comment describing this function
+## list containing function to set & get value of Matrix and Inverse Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x ## returns the value
+  setInvMatrix <- function(invMatrix) m <<-invMatrix ## sets the m value to the inverse matrix
+  getInvMatrix <- function () m ## returns the m value which is the Inverse Matrix
+  list(set= set, get=get, setInvMatrix = setInvMatrix,getInvMatrix = getInvMatrix) ## allows the function to be returned as a list so you can access each method
 
 }
 
 
-## Write a short comment describing this function
-
+## retrieves the inverse if it is already cached if not then it calculates the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInvMatrix()
+  if(!is.null(m)) {   ## returns the inv matrix if it has been calculated already
+    message("retrieving")
+    return(m)
+  }
+  im <- x$get() 
+  m <- solve(im) ## find inverse
+  x$setInvMatrix(m) ## sets the inverse so it can be recalled later on if needed
+  m ## print in console when method is called 
 }
+
